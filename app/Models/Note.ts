d545cbe92a79e8b0be250a81bdae7d7c8636fc1f
Sojeb Sikder <sojebsikder@gmail.com>
2022-05-08 20:20:37 +0600

    main

--- app/Models/Note.ts
+++ app/Models/Note.ts
@@ -5,14 +5,14 @@ export default class Note extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  title: string
+  @column()
+  text: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
-
-  @column()
-  title: string
-  @column()
-  text: string
 }
